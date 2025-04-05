6bc509b4cc103dad72bc75fcb07c536414bd5b1d
Tanvi karale <119518074+TanviKarale3@users.noreply.github.com>
2025-04-05 17:14:44 +0530

    config.py

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ class Config:
     # MySQL Configuration
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = '@vinay0728'
+    MYSQL_PASSWORD = 'Enter Your Pass'
     MYSQL_DB = 'therapytalk'
 
     # Flask-Mail Configuration
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
-    MAIL_USERNAME = 'vinaykakad56@gmail.com'
+    MAIL_USERNAME = 'tanvikarale3@gmail.com'
     MAIL_PASSWORD = 'hxre eoif qqgh jegk'
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
